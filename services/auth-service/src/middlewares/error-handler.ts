6d05967f9912eb6d2730b0b07d059aa2de94362a
Badal <badalsingh120403@gmail.com>
2025-06-08 11:35:43 -0500

    feat(auth): implement custom error handler

---
+++ services/auth-service/src/middlewares/error-handler.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { CustomError } from "../errors";
+import { StatusCodes } from "http-status-codes";
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof CustomError) {
+    res.status(err.statusCode).json({ errors: err.serializeErrors() });
+    return;
+  }
+
+  console.error(err);
+
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    errors: [{ message: "Something went wrong, please try again later" }],
+  });
+};
