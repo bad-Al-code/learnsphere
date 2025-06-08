1097d9933ce6a33bb09835e9e7145492e50bc607
Badal <badalsingh120403@gmail.com>
2025-06-08 11:29:06 -0500

    feat: install express dotenv, http-status-code

--- services/auth-service/src/index.ts
+++ services/auth-service/src/index.ts
@@ -1,2 +1,14 @@
+import express, { Request, Response, json } from "express";
+import { StatusCodes } from "http-status-codes";
+
 const PORT = 3000;
-console.log(PORT);
+const app = express();
+
+app.use(json());
+app.get("/health", (req: Request, res: Response) => {
+  res.status(StatusCodes.OK).json({ status: "UP" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Auth service is running at port ${PORT}`);
+});
