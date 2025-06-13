faa77925dacfdac2b858c779052fdbd5199b4451
Badal <badalsingh120403@gmail.com>
2025-06-13 01:37:54 -0500

    fix(auth): remove unique from passwordHash in users table as two user can have same passwordHash

--- services/auth-service/src/db/schema.ts
+++ services/auth-service/src/db/schema.ts
@@ -4,8 +4,7 @@ import { text, timestamp, uuid, pgTable, boolean } from "drizzle-orm/pg-core";
 export const users = pgTable("users", {
   id: uuid("id").default(sql`gen_random_uuid()`),
   email: text("email").notNull().unique(),
-  // TODO: remove unique frm passwordHash
-  passwordHash: text("password_hash").notNull().unique(),
+  passwordHash: text("password_hash").notNull(),
   isVerified: boolean("is_verified").default(false).notNull(),
   verificationToken: text("verification_token"),
   verificationTokenExpiresAt: timestamp("verification_token_expires_at"),
