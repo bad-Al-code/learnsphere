1f34f9a3e96dcaddd23b82a0b76da867fa5ab963
Badal <badalsingh120403@gmail.com>
2025-06-13 11:53:08 -0500

    fix(media): typo, change auth to media service

--- services/media-service/src/routes/health.ts
+++ services/media-service/src/routes/health.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.get("/health", (req: Request, res: Response) => {
   res
     .status(StatusCodes.OK)
-    .json({ message: "Auth service is up and running" });
+    .json({ message: "Media service is up and running" });
 });
 
 export { router as healthRouter };
