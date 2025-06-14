82fec19e07d0957560083dcde23aa0d4d339d502
Badal <badalsingh120403@gmail.com>
2025-06-14 12:55:27 -0500

    fix(course): add a /api/module/ main route to get all module related endpoint fot clean and standard structure

--- services/course-service/src/app.ts
+++ services/course-service/src/app.ts
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 
 app.use("/api/courses", healthRouter);
 app.use("/api/courses", courseRouter);
-app.use("/api/courses", moduleRouter);
+app.use("/api/modules", moduleRouter);
 
 app.use(errorHandler);
 
