c44a0bc04b870e5780ff05ddc50af648dd801c3f
Badal <badalsingh120403@gmail.com>
2025-06-16 00:19:51 -0500

    feat(iac): add ecr module and create private repositories

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -16,3 +16,27 @@ module "vpc" {
   aws_region = var.aws_region
 }
 
+module "ecr_auth_service" {
+  source          = "./modules/ecr"
+  repository_name = "learnsphere/auth-service"
+}
+
+module "ecr_user_service" {
+  source          = "./modules/ecr"
+  repository_name = "learnsphere/user-service"
+}
+
+module "ecr_media_service" {
+  source          = "./modules/ecr"
+  repository_name = "learnsphere/media-service"
+}
+
+module "ecr_notification_service" {
+  source          = "./modules/ecr"
+  repository_name = "learnsphere/notification-service"
+}
+
+module "ecr_course_service" {
+  source          = "./modules/ecr"
+  repository_name = "learnsphere/course-service"
+}
--- infrastructure/terraform/output.tf
+++ infrastructure/terraform/output.tf
@@ -19,3 +19,14 @@ output "media_service_secret_access_key" {
   value     = aws_iam_access_key.media_service_keys.secret
   sensitive = true
 }
+
+output "ecr_repo_urls" {
+  description = "A map of service names to their ECR repository URLs"
+  value = {
+    auth_service = module.ecr_auth_service.repository_url
+ user_service        = module.ecr_user_service.repository_url
+    media_service       = module.ecr_media_service.repository_url
+    notification_service = module.ecr_notification_service.repository_url
+    course_service      = module.ecr_course_service.repository_url
+  }
+}
