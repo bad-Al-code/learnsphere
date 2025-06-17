38c7a479f04e1eae1a498b0233d95ea058406dbb
Badal <badalsingh120403@gmail.com>
2025-06-16 22:29:36 -0500

    feat(enrollment): add enrollment schema

---
+++ services/enrollment-service/src/db/schema.ts
@@ -0,0 +1,21 @@
+import { uuid, timestamp, unique, pgTable, jsonb } from "drizzle-orm/pg-core";
+
+export const enrollment = pgTable(
+  "enrollments",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    userId: uuid("user_id").notNull(),
+    courseId: uuid("course_id").notNull(),
+    enrolledAt: timestamp("enrolled_at").defaultNow().notNull(),
+
+    progress: jsonb("progress").default({}).notNull(),
+  },
+  (table) => {
+    return {
+      uniqueEnrollment: unique("unique_enrollment").on(
+        table.userId,
+        table.courseId
+      ),
+    };
+  }
+);
