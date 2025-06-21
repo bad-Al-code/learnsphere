0b5f039dd9aa58ca749af6bfb7ea770e05bdb2e4
Badal <badalsingh120403@gmail.com>
2025-06-21 17:55:09 -0500

    feat(media): implement video processing orchestration
    - Updates the VideoProcessor to orchestrate the entire HLS transcoding pipeline
    - Implement private helper methods to download the raw video from S3 to a temporary local directory and upload the processed HLS directory back to s3
    - Integrate the TRanscoderService to perfrm the code video processing
    - Correctly sets mime type for hls file

--- services/media-service/src/workers/processors/video-processor.ts
+++ services/media-service/src/workers/processors/video-processor.ts
@@ -1,11 +1,91 @@
 import { Message } from "@aws-sdk/client-sqs";
 import { IProcessor, S3EventInfo } from "./ip-processor";
-import logger from "../../config/logger";
+import path, { resolve } from "node:path";
+import os from "node:os";
+import fs from "fs-extra";
+import { TranscoderService } from "../../services/transcoder.service";
 import { VideoProcessedPublisher } from "../../events/publisher";
+import logger from "../../config/logger";
+import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
+import { s3Client } from "../../config/s3Client";
+
+const processedBucket = process.env.AWS_PROCESSED_MEDIA_BUCKET!;
 
 export class VideoProcessor implements IProcessor {
   public canProcess(metadata: Record<string, string | undefined>): boolean {
     return metadata.hasOwnProperty("lessonId");
+  }
+
+  private async downloadFile(
+    s3Info: S3EventInfo,
+    tempDir: string
+  ): Promise<string> {
+    const localPath = path.join(tempDir, path.basename(s3Info.key));
+
+    logger.info(
+      `Downloading s3://${s3Info.bucket}/${s3Info.key} to ${localPath}`
+    );
+
+    const getCommand = new GetObjectCommand({
+      Bucket: s3Info.bucket,
+      Key: s3Info.key,
+    });
+    const response = await s3Client.send(getCommand);
+
+    if (!response.Body) {
+      throw new Error(`S3 object has no body`);
+    }
+
+    const writeStream = fs.createWriteStream(localPath);
+    response.Body.transformToWebStream().pipeTo(
+      new WritableStream({
+        write(chunk) {
+          writeStream.write(chunk);
+        },
+        close() {
+          writeStream.end();
+        },
+      })
+    );
+
+    return new Promise((resolve, reject) => {
+      writeStream.on("finish", () => resolve(localPath));
+      writeStream.on("error", reject);
+    });
+  }
+
+  private getMimeType(filename: string): string {
+    if (filename.endsWith(".m3u8")) return "application/vnd.apple.mpegurl";
+    if (filename.endsWith(".ts")) return "video/mp2t";
+    return "application/octet-stream";
+  }
+
+  private async uploadDirectory(
+    dirPath: string,
+    s3KeyPrefix: string
+  ): Promise<void> {
+    const files = await fs.readdir(dirPath);
+    logger.debug(
+      `Uploading ${files.length} files from ${dirPath} to s3://${processedBucket}/${s3KeyPrefix}`
+    );
+
+    const uploadPromises = files.map(async (file) => {
+      const localFilePath = path.join(dirPath, file);
+      const s3Key = `${s3KeyPrefix}/${file}`;
+
+      const putCommand = new PutObjectCommand({
+        Bucket: processedBucket,
+        Key: s3Key,
+        Body: fs.createReadStream(localFilePath),
+        ContentType: this.getMimeType(file),
+      });
+
+      await s3Client.send(putCommand);
+    });
+
+    await Promise.all(uploadPromises);
+
+    logger.info(`Successfully uploaded all HLS files tp ${s3KeyPrefix}`);
   }
 
   public async process(
@@ -15,20 +95,48 @@ export class VideoProcessor implements IProcessor {
   ): Promise<void> {
     const lessonId = metadata.lessonId;
     if (!lessonId) {
-      throw new Error(`VideoProcessor called without a lessonId in metadata.`);
+      throw new Error(`VideoProcess called without a lessonId in metadata`);
     }
 
-    //TODO: Implement video transcoding logic
-    // simulation
-    const fakeVideoUrl = `https://learnsphere-processed-media-xt9tcab6.s3.ap-south-1.amazonaws.com/videos/${lessonId}/playlist.m3u8`;
-    logger.info(
-      `[SIMULATED] Video transcoding complete for lesson: ${lessonId}`
+    const tempRawDir = path.join(
+      os.tmpdir(),
+      `learnsphere-raw-${lessonId}-${Date.now()}`
+    );
+    const tempProcessedDir = path.join(
+      os.tmpdir(),
+      `learnsphere-processed-${lessonId}-${Date.now()}`
     );
 
-    const publisher = new VideoProcessedPublisher();
-    await publisher.publish({
-      lessonId: lessonId,
-      videoUrl: fakeVideoUrl,
-    });
+    await fs.ensureDir(tempRawDir);
+    await fs.ensureDir(tempProcessedDir);
+
+    try {
+      const rawVideoPath = await this.downloadFile(s3Info, tempRawDir);
+
+      await TranscoderService.transcodeToHls({
+        inputPath: rawVideoPath,
+        outputDir: tempProcessedDir,
+      });
+
+      const s3OutputPrefix = `videos/${lessonId}`;
+      await this.uploadDirectory(tempProcessedDir, s3OutputPrefix);
+
+      const finalPlaylistUrl = `https://${processedBucket}.s3.${process.env.AWS_REGION}.amazonaws.com/${s3OutputPrefix}/playlist.m3u8`;
+
+      const publisher = new VideoProcessedPublisher();
+      await publisher.publish({
+        lessonId: lessonId,
+        videoUrl: finalPlaylistUrl,
+      });
+    } catch (error) {
+      logger.error(`Failed to process video for lesson ${lessonId}`, { error });
+
+      throw error;
+    } finally {
+      await fs.remove(tempRawDir);
+      await fs.remove(tempProcessedDir);
+
+      logger.debug(`Cleaned up temporary directions for lesson ${lessonId}`);
+    }
   }
 }
