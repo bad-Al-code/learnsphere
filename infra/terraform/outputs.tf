fe16a2a3c1dd5baee0b5fadb2448e7fb1966be3b
Badal <badalsingh120403@gmail.com>
2025-07-02 21:18:41 -0500

    chore(devops): add output of module in main outputs.tf

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,3 +1,9 @@
+output "aws_region" {
+  description = "AWS region"
+  value       = var.aws_region
+}
+
+
 output "ecr_repository_urls" {
   description = "A map of service names to their ECR repository URLs."
   value = {
@@ -8,4 +14,41 @@ output "ecr_repository_urls" {
     course_service       = module.ecr_course_service.repository_url
     enrollment_service   = module.ecr_enrollment_service.repository_url
   }
+}
+
+
+output "cluster_endpoint" {
+  description = "The endpoint for the EKS cluster's Kubernetes API."
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_name" {
+  description = "The name of the EKS cluster."
+  value       = module.eks.cluster_name
+}
+
+output "cluster_arn" {
+  description = "The ARN of the EKS cluster,"
+  value       = module.eks.cluster_arn
+}
+
+output "cluster_oidc_issuer_url" {
+  description = "The OIDC issuer URL for the EKS cluster,"
+  value       = module.eks.cluster_oidc_issuer_url
+}
+
+
+output "vpc_id" {
+  description = "The ID of the VPC."
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "List of IDs of the public subnets."
+  value       = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "List of IDs of the private subnets."
+  value       = module.vpc.private_subnet_ids
 }
