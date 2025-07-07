4d226d9b2e2387d4cfce9de5dbcb50d81a7bb6a7
Badal <badalsingh120403@gmail.com>
2025-07-07 07:16:03 -0500

    refactor(auth): move all remaining business logic to AuthService

--- services/auth-service/src/controllers/auth.controller.ts
+++ services/auth-service/src/controllers/auth.controller.ts
@@ -18,145 +18,33 @@ export class AuthController {
     return result;
   }
 
-  public static async verifyEmail(email: string, verificationToken: string) {
-    const hashedToken = TokenUtil.hashToken(verificationToken);
-
-    const user = await db.query.users.findFirst({
-      where: (users, { and, eq }) =>
-        and(eq(users.email, email), eq(users.verificationToken, hashedToken)),
-    });
-
-    if (!user) {
-      throw new UnauthenticatedError("Verification failed: Invalid token");
-    }
-
-    if (
-      !user.verificationTokenExpiresAt ||
-      Date.now() > user.verificationTokenExpiresAt.getTime()
-    ) {
-      logger.warn(
-        `Attempt to use expired or missing verification token for user: ${user.id}`
-      );
-      throw new UnauthenticatedError("Verification failed: Token has expired.");
-    }
-
-    logger.info(`Verifying email for user ID: ${user.id}`);
-
-    await db
-      .update(users)
-      .set({
-        isVerified: true,
-        verificationToken: null,
-        verificationTokenExpiresAt: null,
-      })
-      .where(eq(users.id, user.id!));
-
-    logger.info(`Email successfully verified for user ID: ${user.id}`);
-  }
-
   public static async login(email: string, password: string) {
     const user = await AuthService.login(email, password);
 
     return user;
   }
 
+  public static async verifyEmail(email: string, verificationToken: string) {
+    await AuthService.verifyEmail(email, verificationToken);
+  }
+
   public static async forgotPassword(email: string) {
-    const user = await db.query.users.findFirst({
-      where: eq(users.email, email),
-    });
-    if (!user) {
-      logger.warn(`Password reset requested for non-existent email: ${email}`);
-      return null;
-    }
+    const result = await AuthService.forgotPassword(email);
 
-    const { rawToken: resetToken, hashedToken } =
-      TokenUtil.generateHashedToken();
-    const passwordResetTokenExpiresAt = TokenUtil.getExpirationDate(
-      FIFTEEN_MINUTES_IN_MS
-    );
-
-    await db
-      .update(users)
-      .set({
-        passwordResetToken: hashedToken,
-        passwordResetTokenExpiresAt,
-      })
-      .where(eq(users.id, user.id!));
-
-    logger.info(`Password reset token generated for user: ${user.id}`);
-
-    return { resetToken };
+    return result;
   }
 
   public static async resetPassword(
     email: string,
-    resetToken: string,
-    newPassword: string
+    token: string,
+    password: string
   ) {
-    const hashedToken = TokenUtil.hashToken(resetToken);
-
-    const user = await db.query.users.findFirst({
-      where: (users, { and, eq }) =>
-        and(eq(users.email, email), eq(users.passwordResetToken, hashedToken)),
-    });
-
-    if (!user) {
-      throw new UnauthenticatedError("Password reset failed: Invalid token.");
-    }
-
-    if (
-      !user.passwordResetTokenExpiresAt ||
-      Date.now() > user.passwordResetTokenExpiresAt.getTime()
-    ) {
-      throw new UnauthenticatedError(
-        "Password reset failed: Token has expired"
-      );
-    }
-
-    const newPasswordHash = await Password.toHash(newPassword);
-
-    await db
-      .update(users)
-      .set({
-        passwordHash: newPasswordHash,
-        passwordResetToken: null,
-        passwordResetTokenExpiresAt: null,
-        passwordChangedAt: new Date(),
-        updatedAt: new Date(),
-      })
-      .where(eq(users.id, user.id!));
-
-    logger.info(`Password succesfully reset for user ID: ${user.id}`);
+    await AuthService.resetPassword(email, token, password);
   }
 
   public static async resendVerificationEmail(email: string) {
-    const user = await db.query.users.findFirst({
-      where: eq(users.email, email),
-    });
-    if (!user || user.isVerified) {
-      if (user?.isVerified)
-        logger.info(
-          `Verification resend requested for already verified user: ${email}`
-        );
+    const result = await AuthService.resendVerificationEmail(email);
 
-      return null;
-    }
-
-    const { rawToken: verificationToken, hashedToken } =
-      TokenUtil.generateHashedToken();
-    const verificationTokenExpiresAt =
-      TokenUtil.getExpirationDate(TWO_HOURS_IN_MS);
-
-    await db
-      .update(users)
-      .set({
-        verificationToken: hashedToken,
-        verificationTokenExpiresAt,
-      })
-      .where(eq(users.id, user.id!));
-
-    logger.info(`New verifiation token generated for user: ${user.id}`);
-
-    return { user, verificationToken };
+    return result;
   }
 }
--- services/auth-service/src/services/auth.service.ts
+++ services/auth-service/src/services/auth.service.ts
@@ -4,7 +4,7 @@ import { db } from "../db";
 import { User } from "../db/database.types";
 import { users } from "../db/schema";
 import logger from "../config/logger";
-import { BadRequestError } from "../errors";
+import { BadRequestError, UnauthenticatedError } from "../errors";
 import { TokenUtil } from "../utils/crypto";
 import { Password } from "../utils/password";
 
@@ -74,5 +74,137 @@ export class AuthService {
     }
 
     return existingUser;
+  }
+
+  public static async verifyEmail(email: string, verificationToken: string) {
+    const hashedToken = TokenUtil.hashToken(verificationToken);
+
+    const user = await db.query.users.findFirst({
+      where: (users, { and, eq }) =>
+        and(eq(users.email, email), eq(users.verificationToken, hashedToken)),
+    });
+
+    if (!user) {
+      throw new UnauthenticatedError("Verification failed: Invalid token");
+    }
+
+    if (
+      !user.verificationTokenExpiresAt ||
+      Date.now() > user.verificationTokenExpiresAt.getTime()
+    ) {
+      logger.warn(
+        `Attempt to use expired or missing verification token for user: ${user.id}`
+      );
+      throw new UnauthenticatedError("Verification failed: Token has expired.");
+    }
+
+    logger.info(`Verifying email for user ID: ${user.id}`);
+
+    await db
+      .update(users)
+      .set({
+        isVerified: true,
+        verificationToken: null,
+        verificationTokenExpiresAt: null,
+      })
+      .where(eq(users.id, user.id!));
+
+    logger.info(`Email successfully verified for user ID: ${user.id}`);
+  }
+
+  public static async forgotPassword(email: string) {
+    const user = await this._findUserByEmail(email);
+    if (!user) {
+      logger.warn(`Password reset requested for non-existent email: ${email}`);
+      return null;
+    }
+
+    const { rawToken: resetToken, hashedToken } =
+      TokenUtil.generateHashedToken();
+    const passwordResetTokenExpiresAt = TokenUtil.getExpirationDate(
+      FIFTEEN_MINUTES_IN_MS
+    );
+
+    await db
+      .update(users)
+      .set({
+        passwordResetToken: hashedToken,
+        passwordResetTokenExpiresAt,
+      })
+      .where(eq(users.id, user.id!));
+
+    logger.info(`Password reset token generated for user: ${user.id}`);
+
+    return { resetToken };
+  }
+
+  public static async resetPassword(
+    email: string,
+    resetToken: string,
+    newPassword: string
+  ) {
+    const hashedToken = TokenUtil.hashToken(resetToken);
+
+    const user = await db.query.users.findFirst({
+      where: (users, { and, eq }) =>
+        and(eq(users.email, email), eq(users.passwordResetToken, hashedToken)),
+    });
+
+    if (!user) {
+      throw new UnauthenticatedError("Password reset failed: Invalid token.");
+    }
+
+    if (
+      !user.passwordResetTokenExpiresAt ||
+      Date.now() > user.passwordResetTokenExpiresAt.getTime()
+    ) {
+      throw new UnauthenticatedError(
+        "Password reset failed: Token has expired"
+      );
+    }
+
+    const newPasswordHash = await Password.toHash(newPassword);
+
+    await db
+      .update(users)
+      .set({
+        passwordHash: newPasswordHash,
+        passwordResetToken: null,
+        passwordResetTokenExpiresAt: null,
+        passwordChangedAt: new Date(),
+        updatedAt: new Date(),
+      })
+      .where(eq(users.id, user.id!));
+
+    logger.info(`Password succesfully reset for user ID: ${user.id}`);
+  }
+
+  public static async resendVerificationEmail(email: string) {
+    const user = await this._findUserByEmail(email);
+    if (!user || user.isVerified) {
+      if (user?.isVerified)
+        logger.info(
+          `Verification resend requested for already verified user: ${email}`
+        );
+
+      return null;
+    }
+
+    const { rawToken: verificationToken, hashedToken } =
+      TokenUtil.generateHashedToken();
+    const verificationTokenExpiresAt =
+      TokenUtil.getExpirationDate(TWO_HOURS_IN_MS);
+
+    await db
+      .update(users)
+      .set({
+        verificationToken: hashedToken,
+        verificationTokenExpiresAt,
+      })
+      .where(eq(users.id, user.id!));
+
+    logger.info(`New verifiation token generated for user: ${user.id}`);
+
+    return { user, verificationToken };
   }
 }
