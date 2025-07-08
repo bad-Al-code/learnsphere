cac056d48c1bef51f653c637317c716eaebba026
Badal <badalsingh120403@gmail.com>
2025-07-08 04:40:02 -0500

    docs(auth): update readme

--- services/auth-service/src/middlewares/rate-limiter.ts
+++ services/auth-service/src/middlewares/rate-limiter.ts
@@ -20,7 +20,7 @@ function createLimiter(): RateLimitRequestHandler {
 
   limiterInstance = rateLimit({
     windowMs: 15 * 60 * 1000, // 15min
-    max: 200,
+    max: 20,
     standardHeaders: true,
     legacyHeaders: false,
     store: store,
