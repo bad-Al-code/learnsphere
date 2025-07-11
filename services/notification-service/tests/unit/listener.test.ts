861ae7607e087a9f31178f35d55e3475ab8b2448
Badal <badalsingh120403@gmail.com>
2025-07-11 05:52:51 -0500

    test(notification): add test for UserPasswordResetRequiredListener

--- services/notification-service/tests/unit/listener.test.ts
+++ services/notification-service/tests/unit/listener.test.ts
@@ -2,7 +2,10 @@ import { describe, it, expect, beforeEach, vi } from 'vitest';
 import { faker } from '@faker-js/faker';
 
 import { EmailService } from '../../src/services/email-service';
-import { UserVerificationRequiredListener } from '../../src/events/listener';
+import {
+  UserPasswordResetRequiredListener,
+  UserVerificationRequiredListener,
+} from '../../src/events/listener';
 import { EmailClient } from '../../src/clients/email.client';
 
 vi.mock('../../src/services/email-service');
@@ -30,6 +33,34 @@ describe('UserVerificationRequiredListener', () => {
 
     expect(mockEmailService.sendVerificationEmail).toHaveBeenCalledTimes(1);
     expect(mockEmailService.sendVerificationEmail).toHaveBeenCalledWith(
+      eventData
+    );
+  });
+});
+
+describe('UserPasswordResetRequiredListener', () => {
+  let mockEmailService: EmailService;
+  let listener: UserPasswordResetRequiredListener;
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+
+    mockEmailService = new EmailService(new EmailClient());
+
+    listener = new UserPasswordResetRequiredListener(mockEmailService);
+  });
+
+  it('should call emailService.sendPasswordResetEmail with the correct data from the message', async () => {
+    const eventData = {
+      email: faker.internet.email(),
+      resetToken: faker.string.uuid(),
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    await listener.onMessage(eventData, null as any);
+
+    expect(mockEmailService.sendPasswordResetEmail).toHaveBeenCalledTimes(1);
+    expect(mockEmailService.sendPasswordResetEmail).toHaveBeenCalledWith(
       eventData
     );
   });
