a54d92588fbebc74f27709226d83b1ce755d14cb
Badal <badalsingh120403@gmail.com>
2025-07-14 06:05:32 -0500

    feat(course): set up swagger and openapi for api documentation

--- services/course-service/src/app.ts
+++ services/course-service/src/app.ts
@@ -1,6 +1,7 @@
 import express, { json } from 'express';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+import swaggerUi from 'swagger-ui-express';
 
 import {
   courseRouter,
@@ -12,10 +13,14 @@ import { currentUser } from './middlewares/current-user';
 import { errorHandler } from './middlewares/error-handler';
 import { httpLogger } from './middlewares/http-logger';
 import { env } from './config/env';
+import { swaggerSpec } from './config/swagger';
 
 const app = express();
 
 app.set('trust proxy', true);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use(json());
 app.use(helmet());
 app.use(httpLogger);
