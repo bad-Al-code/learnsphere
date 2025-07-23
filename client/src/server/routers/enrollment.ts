dd7385f1ca5b28451f657081709961edb358336b
Badal <badalsingh120403@gmail.com>
2025-07-23 05:18:38 +0000

    remove: start client from scratch

--- client/src/server/routers/enrollment.ts
+++
@@ -1,3 +0,0 @@
-import { router } from "../trpc";
-
-export const enrollmentRouter = router({});
--- client/src/server/routers/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./auth";
-export * from "./course";
-export * from "./enrollment";
-export * from "./media";
-export * from "./notification";
-export * from "./user";
